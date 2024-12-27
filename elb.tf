26c9efcb80cec1855f32c22c677d7d5d425077ed
Nandeesh94 <nandeesh.flipkart@gmail.com>
2024-12-27 20:11:18 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -5,24 +5,24 @@ availability_zones = ["us-east-1a", "us-east-1b"]
 
 listener {
 instance_port = 80
-instance_protocol = "http"
+instance_protocol = "HTTP"
 lb_port = 80
-lb_protocol = "http"
+lb_protocol = "HTTP"
 }
 
 health_check {
-healthy_threshould = 3
+healthy_threshold = 3
 unhealthy_threshold = 5
 timeout = 5
-target = "http:80/"
+target = "HTTP:80/"
 interval = 30
 }
 
-instances =["${aws_instance.one.id", "${aws_instance.two.id}"]
+instances = ["${aws_instance.one.id}", "${aws_instance.two.id}"]
 cross_zone_load_balancing = true
 idle_timeout = 400
 tags = {
 Name = "nandu-terraf-elb"
 }
-}
+
 
