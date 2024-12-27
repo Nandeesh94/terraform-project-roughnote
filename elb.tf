a0e2947affbb75e57663c4d05a13540586d0c5ce
Nandeesh94 <nandeesh.flipkart@gmail.com>
2024-12-27 20:12:03 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,28 +1,31 @@
 resource "aws_elb" "bar" {
-name = "nandu-terraform-elb"
-availability_zones = ["us-east-1a", "us-east-1b"]
+  name               = "nandu-terraform-elb"
+  availability_zones = ["us-east-1a", "us-east-1b"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "HTTP"
+    lb_port           = 80
+    lb_protocol       = "HTTP"
+  }
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+    timeout             = 5
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances = [
+    aws_instance.one.id,
+    aws_instance.two.id
+  ]
+
+  cross_zone_load_balancing = true
+  idle_timeout              = 400
+
+  tags = {
+    Name = "nandu-terraf-elb"
+  }
 }
-
-listener {
-instance_port = 80
-instance_protocol = "HTTP"
-lb_port = 80
-lb_protocol = "HTTP"
-}
-
-health_check {
-healthy_threshold = 3
-unhealthy_threshold = 5
-timeout = 5
-target = "HTTP:80/"
-interval = 30
-}
-
-instances = ["${aws_instance.one.id}", "${aws_instance.two.id}"]
-cross_zone_load_balancing = true
-idle_timeout = 400
-tags = {
-Name = "nandu-terraf-elb"
-}
-
-
